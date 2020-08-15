fb164b05960a48b29ed5e31d8920756975b1daa3
rawsky <kyaw.s.thein@gmail.com>
2020-08-15 18:22:37 +0800

    Create writeUpload.py

---
+++ src/showa/modules/writeUpload.py
@@ -0,0 +1,70 @@
+from showa.modules import dataProcess, plotting, config, cmdMake
+from showa.lib.database import Postgres
+from showa.lib import logs
+import pandas as pd
+
+
+# return clean dataframe
+def write_readSer(comPort, ser):
+    print("reading speed..")
+    speedList = dataProcess.write_speed(cmdMake.speedCmd1(),
+                                        cmdMake.speedCmd2(), comPort, ser)
+    print("reading torque..")
+    torqueList = dataProcess.write_torque(cmdMake.torqueCmd1(),
+                                          cmdMake.torqueCmd2(), ser)
+    cleanSpeedList = dataProcess.cleanSpeeddata(speedList)
+    cleanTorqueList = dataProcess.cleanTorquedata(torqueList)
+    # combine lists to form a dictionary
+    tempDict = {'Speed': cleanSpeedList, 'Torque': cleanTorqueList}
+    df = pd.DataFrame(tempDict)
+    return(df)
+
+
+def dfToPlot(df, comPort, dateTime, line):
+    print("plotting..")
+    dfSpeed = df[['Speed']]
+    dfSpeed = dfSpeed.astype(float)
+    dfTorque = df[['Torque']]
+    dfTorque = dfTorque.astype(float)
+    dfRecon = pd.concat([dfSpeed, dfTorque], axis=1)
+    dfRecon.insert(0, 'TimeStamp', pd.datetime.now().replace(microsecond=0))
+    dfRecon.insert(1, 'Line', line)
+    dfRecon.insert(2, 'Chamber', comPort)
+    if config.plotEnable is True:
+        ngGraph, tmax, tmin, CSRa = plotting.plot(dfSpeed, dfTorque, comPort, dateTime, line)
+    else:
+        pass
+    if config.csvEnable is True:
+        dfRecon.to_csv("{}/{}.csv".format(plotting.newDir(dateTime, line),
+                                          plotting.picName(comPort, line)), sep=';', index=True, mode='w')
+    else:
+        pass
+    return(dfRecon, ngGraph, tmax, tmin, CSRa)
+
+
+def uploadDB(df, comPort):
+    myPg = None
+    try:
+        myPg = Postgres(config.url, config.database, config.user,
+                        config.password)
+        myPg.connect()
+        print("Connection succeeded..")
+        listy = df.to_csv(None, header=False, index=False).split('\n')
+        vals = [','.join(ele.split()) for ele in listy]
+        for i in vals:
+            y = i.split(",")
+            y[0: 2] = [' '.join(y[0: 2])]
+            x = tuple(y)
+            if x == ('',):
+                pass
+            else:
+                strSQL = "INSERT INTO tdu.{} values {}".format(
+                         config.schema, x)
+                myPg.execute(strSQL)
+        myPg.commit()
+    except Exception as err:
+        logs.logError(
+            "Upload Error Occured: {}".format(str(err)), includeErrorLine=True)
+    finally:
+        if myPg is not None:
+            myPg.close()
