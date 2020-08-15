d9106d6b80c45663e396ea3654a2224aa49b7408
rawsky <kyaw.s.thein@gmail.com>
2020-08-15 18:27:27 +0800

    Create cmdMake.py

---
+++ src/showa/modules/cmdMake.py
@@ -0,0 +1,63 @@
+import binascii
+
+
+# ASCII to HEX conversion
+def convertAscii_Hex(cd):
+    x = bytes(cd, 'utf-8')
+    y = str(binascii.hexlify(x), 'ascii')
+    # cd = bytearray(' '.join(a+b for a,b in zip(y[::2], y[1::2])), 'utf-8')
+    cd = ' '.join(a+b for a, b in zip(y[::2], y[1::2]))
+    return(cd)
+
+
+# Create command string from command input and data input,
+# Checksum is added
+def cmdStr(inputC, inputD):
+    SOH = '01 '
+    STX = ' 02 '
+    ETX = ' 03'
+    a = convertAscii_Hex(inputC).split()
+    b = convertAscii_Hex(inputD).split()
+    for i in range(0, len(a)):
+        a[i] = int(a[i], 16)
+    for i in range(0, len(b)):
+        b[i] = int(b[i], 16)
+    total = int(STX, 16)+int(ETX, 16) + sum(a) + sum(b)
+    dummy1 = str(hex(total))
+    checksum = convertAscii_Hex(''.join((dummy1[-2:]).split()).upper())
+    stringy = bytearray.fromhex(SOH+convertAscii_Hex(inputC)+STX +
+                                convertAscii_Hex(inputD)+ETX+" "+checksum)
+    return(stringy)
+
+
+def checkerCmd():
+    return cmdStr("036", "00")
+
+
+def dummyCmd():
+    return cmdStr("005", "00")
+
+
+def fetchCmd():
+    data_str = ([hex(i)[2:].zfill(2).upper() for i in range(256)])
+    fetchCmdLst = []
+    for i in data_str:
+        fetchCmd = cmdStr("037", i)
+        fetchCmdLst.append(fetchCmd)
+    return(fetchCmdLst)
+
+
+def speedCmd1():
+    return cmdStr("0B6", "0082FFFFFF00FF")
+
+
+def speedCmd2():
+    return cmdStr("036", "81")
+
+
+def torqueCmd1():
+    return cmdStr("0B6", "0082FFFFFF01FF")
+
+
+def torqueCmd2():
+    return cmdStr("036", "82")
